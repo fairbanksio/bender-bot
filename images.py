fed0128e27c7cca008872a27dd7215442c85a06d
Jon Fairbanks <fairplay89@gmail.com>
2023-03-23 16:10:13 -0700

    Handle error cases when generating images

--- images.py
+++ images.py
@@ -1,12 +1,23 @@
 import replicate
 
+from log_config import logger
+
 # Setup Replicate (Image Generation)
 model = replicate.models.get("stability-ai/stable-diffusion")
 version = model.versions.get("db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf")
 
-def generate_image(prompt):
+def generate_image(image_prompt):
+    """
+    Generates an image based on the provided prompt using the Replicate library.
+
+    Args:
+        image_prompt (str): The prompt to use for image generation.
+
+    Returns:
+        The generated image as a numpy array.
+    """
     inputs = {
-        'prompt': prompt,
+        'prompt': image_prompt,
         'image_dimensions': "768x768",
         'num_outputs': 1,
         'num_inference_steps': 50,
@@ -14,6 +25,12 @@ def generate_image(prompt):
         'scheduler': "DPMSolverMultistep"
     }
 
-    image = version.predict(**inputs)
+    with version:
+        try:
+            image = version.predict(**inputs)
+        except Exception as e:
+            # Handle any exceptions that arise during image generation.
+            logger.error(f"Error generating image: {e}")
+            return None
     
     return image[0]
