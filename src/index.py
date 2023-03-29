2233e443750e3241072eec9cd33a0d4b79b10396
Jon Fairbanks <fairplay89@gmail.com>
2023-03-29 12:02:14 -0700

    Better messaging for context reset

--- src/index.py
+++ src/index.py
@@ -96,7 +96,7 @@ def generate(say, body):
 def reset_context(body, say):
     channel_id = body["channel_id"]
     context.CHAT_CONTEXT[channel_id].clear()
-    say("Done ✅")  # Should probably be a private message
+    say("Hmm, I forgot what we were talking about 🤔")  # Should probably be a private message
 
 
 # Catch all (should be last handler)
